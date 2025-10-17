7606d624752b898c66caa7fe2f8e7108ab006164
SACHIN BURNWAL <sachinburnwal22@gmail.com>
2025-10-17 10:56:20 +0530

    Added content in index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-  
+const express = require("express");
+const setupAndStartServer = async () => {
+  //Create the express object
+  const app = express();
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
+};
